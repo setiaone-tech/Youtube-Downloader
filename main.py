326732f4804c2465608e4b3ab5867fce0da3d3ca
bang L <71912420+setiaone-tech@users.noreply.github.com>
2020-12-19 05:49:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import json
 import telebot
 
-api = "Api-Key Telegram Bot"
+api = "Api-Key"
 bot = telebot.TeleBot(api,threaded=False)
 
 @bot.message_handler(commands=['start'])
@@ -27,10 +27,21 @@ def send_search(message):
     num = 0
     markup = telebot.types.InlineKeyboardMarkup()
     if url['status'] == 200:
-        for i in url['result']:
+        for i in range(10):
             num += 1
-            judul += str(num)+"."+str(i['title'])+"\n"
-            markup.add(telebot.types.InlineKeyboardButton(text=num, callback_data=i['link']))
+            judul += str(num)+"."+str(url['result'][i]['title'])+" "+str(url['result'][i]['duration'])+"\n"
+        id1 = telebot.types.InlineKeyboardButton(text="1", callback_data=url['result'][0]['link'])
+        id2 = telebot.types.InlineKeyboardButton(text="2", callback_data=url['result'][1]['link'])
+        id3 = telebot.types.InlineKeyboardButton(text="3", callback_data=url['result'][2]['link'])
+        id4 = telebot.types.InlineKeyboardButton(text="4", callback_data=url['result'][3]['link'])
+        id5 = telebot.types.InlineKeyboardButton(text="5", callback_data=url['result'][4]['link'])
+        id6 = telebot.types.InlineKeyboardButton(text="6", callback_data=url['result'][5]['link'])
+        id7 = telebot.types.InlineKeyboardButton(text="7", callback_data=url['result'][6]['link'])
+        id8 = telebot.types.InlineKeyboardButton(text="8", callback_data=url['result'][7]['link'])
+        id9 = telebot.types.InlineKeyboardButton(text="9", callback_data=url['result'][8]['link'])
+        id10 = telebot.types.InlineKeyboardButton(text="10", callback_data=url['result'][9]['link'])
+        markup.row(id1,id2,id3,id4,id5)
+        markup.row(id6,id7,id8,id9,id10)
         bot.send_message(chat_id, text=judul, reply_markup = markup)
     else:
         bot.send_message(chat_id, url['error'])
@@ -41,14 +52,14 @@ def handler(call):
     url = requests.get('https://mhankbarbars.herokuapp.com/api/ytv?url='+cari+'&apiKey=API-KEY').json()
     bot.send_message(call.from_user.id, "Permintaan anda sedang di proses...")
     if url['status'] == 200:
-        bot.send_video(call.from_user.id, url['result'])
+        bot.send_video(call.from_user.id, url['result'], caption = "title : "+str(url['title'])+"\nFileSize : "+str(url['filesize']))
     else:
-        bot.send_message(call.from_user.id, url['error']
+        bot.send_message(call.from_user.id, url['error'])
         
 @bot.message_handler(commands=['about'])
 def send_about(message):
         chat_id = message.chat.id
-        bot.send_message(chat_id, "Author : Tia:)\n Thanks For : Feri <3"
+        bot.send_message(chat_id, "BOT ini dibuat menggunakan bahasa pemrogram Python.\nBOT ini dibuat dikala kegabutan melanda.\n\nAuthor : Tia\nSpecial Thanks : pyTelegramBotAPI Indonesia")
 
 while True:
   try:
